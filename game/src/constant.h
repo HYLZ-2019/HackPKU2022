62997b27a08b5394ac0a05c75ff10f5adf5e1c6f
none44353 <54280222+none44353@users.noreply.github.com>
2022-05-20 17:27:56 +0800

    add MAX_HEIGHT

--- game/src/constant.h
+++ game/src/constant.h
@@ -1,9 +1,13 @@
 #include <cmath>
 
 const int FPS = 60; // 游戏的帧率
+const double Pi = acos(-1);
 
 // TODO(wy) : 修改至你喜欢的大小
 const int BLOCK_NUMBER = 720; // 2pi被分割成多少个角度
+const double UP_STEP_LENGTH = 0.5;
+const double DOWN_STEP_LENGTH = 1;
+const double MAX_HEIGHT = 50;
 
 // TODO(xy) : 给出你觉得合适的判定点大小
 const int NOTE_HEIGHT = 5;
